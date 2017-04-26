4bee21f71601a187b13269b0b2900d75d6605104
Joel <waylaidwanderer@live.com>
2017-04-26 14:19:47 -0700

    Implement logging in and setting cookies

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,13 @@ module.exports = {
     "extends": "airbnb-base",
     "plugins": [
         "import"
-    ]
+    ],
+    "rules": {
+        "linebreak-style": 0,
+        "no-console": 0,
+        "no-use-before-define": ["error", "nofunc"],
+        "indent": ["error", 4],
+        "arrow-parens": [2, "as-needed", { "requireForBlockBody": true }],
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }]
+    }
 };
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const config = require('./config');
+const request = require('request');
 const SteamClient = require('steam-client');
+const SteamCommunity = require('steamcommunity');
 const SteamUser = require('steam-user');
 const SteamTotp = require('steam-totp');
 const TradeOfferManager = require('steam-tradeoffer-manager');
 
-//noinspection JSIgnoredPromiseFromCall
+// noinspection JSIgnoredPromiseFromCall
 main();
 
 async function main() {
@@ -14,29 +16,97 @@ async function main() {
     }
     const client = new SteamUser(steam);
     console.log(`Logging into ${config.username}...`);
-    await loginToAccount(client, config);
-    console.log(`Logged in with IP ${client.publicIP}!`);
+    await loginToSteamClient(client, config);
+    console.log(`Logged into Steam client with IP ${client.publicIP}!`);
+    const community = new SteamCommunity({
+        request: request.defaults({
+            proxy: config.proxy ? `http://${config.proxy}` : undefined,
+        }),
+    });
     const manager = new TradeOfferManager({
         steam: client,
-        language: 'en'
+        community,
+        language: 'en',
+        cancelTime: 5 * 60 * 1000,
     });
+    console.log('Waiting 30s...');
+    await new Promise(resolve => setTimeout(resolve, 30 * 1000));
+    await retryLogin(community, manager);
     // TODO
 }
 
-async function loginToAccount(client, config) {
+async function loginToSteamClient(client) {
+    const code = await generateAuthCode();
+    console.log(`Using 2FA code "${code}".`);
     client.logOn({
         accountName: config.username,
         password: config.password,
-        twoFactorCode: SteamTotp.generateAuthCode(config.shared_secret)
+        twoFactorCode: code,
     });
     // TODO: handle invalid 2fa code
     await waitForEvent(client, 'loggedOn');
 }
 
-function waitForEvent(client, name) {
-    return new Promise(resolve => {
-        client.on(name, () => {
-            resolve();
+async function loginToSteamCommunity(community) {
+    const code = await generateAuthCode();
+    console.log(`Using 2FA code "${code}".`);
+    return new Promise((resolve, reject) => {
+        community.login({
+            accountName: config.username,
+            password: config.password,
+            twoFactorCode: code,
+        }, (err, sessionID, cookies) => {
+            if (err) return reject(err);
+            return resolve(cookies);
         });
+    });
+}
+
+/* eslint-disable no-await-in-loop */
+async function retryLogin(community, tradeOfferManager) {
+    console.log('Logging into Steam Community website...');
+    for (let i = 0; i < 3; i++) {
+        try {
+            const cookies = await loginToSteamCommunity(community);
+            await setCookies(tradeOfferManager, cookies);
+            console.log('Successfully logged in!');
+            return Promise.resolve();
+        } catch (err) {
+            if (err.toString().includes('SteamGuardMobile')) {
+                console.log(err.message);
+            } else {
+                console.log(err);
+            }
+            await new Promise(resolve => setTimeout(resolve, 2500));
+        }
+    }
+    console.log('Can\'t login to account! Waiting a minute before trying again...');
+    await new Promise(resolve => setTimeout(resolve, 60 * 1000));
+    await retryLogin(community, tradeOfferManager);
+    return Promise.resolve();
+}
+/* eslint-enable no-await-in-loop */
+
+async function generateAuthCode() {
+    return new Promise((resolve, reject) => {
+        SteamTotp.generateAuthCode(config.shared_secret, (err, code) => {
+            if (err) return reject(err);
+            return resolve(code);
+        });
+    });
+}
+
+async function setCookies(tradeOfferManager, cookies) {
+    return new Promise((resolve, reject) => {
+        tradeOfferManager.setCookies(cookies, null, (err) => {
+            if (err) return reject(err);
+            return resolve();
+        });
+    });
+}
+
+function waitForEvent(client, name) {
+    return new Promise((resolve) => {
+        client.on(name, resolve);
     });
 }
